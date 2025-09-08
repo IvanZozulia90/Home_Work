cf282831c53f465ed3e7a8f92a40872f25e7dee6
Ivan <ivan.zozulia90@gmail.com>
2025-09-08 10:20:26 +0300

    add HW2

--- HW_2.js
+++ HW_2.js
@@ -1,17 +1,67 @@
 // let sum = 2*2*2;
 
-const x = 2;
-const y = 3;
+// const x = 2;
+// const y = 3;
 
-function pow(x, y) {
-    let result = x;
-    for (let i = 1; i < y; i++){
-        result *= x;
+// function pow(x, y) {
+//     let result = x;
+//     for (let i = 1; i < y; i++){
+//         result *= x;
+//     }
+//     return result
+// };
+
+// let answer = pow(x,y);
+// console.log(answer);
+
+
+// function sayHello(){
+//     return ("Привіт, JavaScript!");
+// }
+// console.log(sayHello())
+
+
+// const say = 'Hellow'
+
+// function sayHello(say){
+//     return(say === "Привіт") ? "Привіт, JavaScript!": "Допобачення"; 
+// }
+
+// let answer = sayHello(say);
+// console.log(answer);
+
+
+// let name = 'Ivan';
+
+// function greet(name) {
+//     return (`Привіт ${name}`)
+// };
+
+// console.log(greet(name));
+
+// let a = 100;
+// let b = 200;
+
+// function sum (a, b){
+//     return a + b;
+// };
+// console.log(sum(a, b));
+
+
+// let age = 17;
+
+// function isAdult(age){
+//     return (age >= 18) ? "Повнолітній" : "Дитина";
+// };
+// console.log(isAdult(age));
+
+const n = 10;
+
+
+function printNumbers(n){
+    for (let i=1; i<=n; i++){
+        console.log(i)
     }
-    return result
-};
+}
 
-let answer = pow(x,y);
-console.log(answer);
-
-
+console.log(printNumbers(n))
