380d8833a0473a619da41acc3a90b2efb844a5af
Ivan <ivan.zozulia90@gmail.com>
2025-10-06 22:35:21 +0300

    fix

--- HW_5.2.js
+++ HW_5.2.js
@@ -7,7 +7,7 @@ let arr = [
     {
         userName:"Dmitro",
         lastName:"Porohov",
-        email:"dmitro.porohov@yahoo.com>"
+        email:"dmitro.porohov@yahoo.com"
     },
     {
         userName:"Andrii",
@@ -22,13 +22,10 @@ let arr = [
 function searchMail () {
     let mail = [];
     let validator = /^\w+(\.\w+)@(gmail\.com|yahoo\.com)$/
-    // let search = /@gmail|@yahoo/;
+    
     for (let i = 0; i < arr.length; i++){
         if(validator.test(arr[i].email)){
-            mail.push({
-                good_email: arr[i].email,
-                userName: arr[i].userName
-            });
+            mail.push(arr[i].email);
         }
     }
     return mail
